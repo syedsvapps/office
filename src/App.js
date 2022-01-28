e6b1b6716d88ef2728553538d60e5b33547c0610
Dilip Kumar <dilipkumar.mattewada@gmail.com>
2022-01-28 18:55:04 +0530

    creating new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <BrowserRouter>
+
+  <Navbar/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>welcome to reactjs project</h1>
+        {/* <Route path="/home" component={Home} /> */}
+
     </div>
+    <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React,{ Component} from 'react';
+
+class Navbar extends Component
+{
+    render(){
+        return (
+            <h1>navbar </h1>
+           
+          );
+          
+    }
+}
+
+export default Navbar;
